3eb89833ec6d0ae1690ee36946325922a2053c81
luosch <i@lsich.com>
2022-06-23 16:56:55 +0800

    [Add] Initial Project

---
+++ MetaMaskDemo/MetaMaskDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetaMaskDemoApp.swift
+//  MetaMaskDemo
+//
+//  Created by Danny Luo on 2022/6/23.
+//
+
+import SwiftUI
+
+@main
+struct MetaMaskDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
